dc98b1eb6d9e881e65e7939c9c977fe42c184041
Seregey Dubovets <dubovets@ok.by>
2023-12-12 22:43:03 +0300

    Jenkinsfile: Fixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,8 +12,8 @@ terraform {
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "dos15-dubovets-gd-terraform-lock"
-    access_key     = var.access_key
-    secret_key     = var.secret_key
+    access_key     = "AKIAU4L6PJKVGIIMIY4P"
+    secret_key     = "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"
   }
 }
 
