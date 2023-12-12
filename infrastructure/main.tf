1316f1c4625f8e3fd4ebf9da53033822cdbb0fc0
Seregey Dubovets <dubovets@ok.by>
2023-12-12 23:16:33 +0300

    Terraform: Fixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,8 +12,6 @@ terraform {
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "dos15-dubovets-gd-terraform-lock"
-    access_key     = "AKIAU4L6PJKVGIIMIY4P"
-    secret_key     = "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"
   }
 }
 
