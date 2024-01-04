5dd1909dc1e1aa8d4264a0e8022ad0bbccd74ac5
Seregey Dubovets <dubovets@ok.by>
2024-01-04 04:48:09 +0300

    Terraform: Security group

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.name}-sg"
   }
