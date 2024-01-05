686a49affc44298a1cfc6d4c02b67215fc1e96d4
Seregey Dubovets <dubovets@ok.by>
2024-01-05 05:06:29 +0300

    Jenkins: Adding hosts to known hosts

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -23,8 +23,8 @@ resource "aws_security_group" "main" {
   }
 
   egress {
-    from_port   = 57385
-    to_port     = 57385
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
